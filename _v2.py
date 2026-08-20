1252339c818a752be02475a2a9dac848ff1f76f5
Madao03 <98048020+Madao03@users.noreply.github.com>
2026-08-20 20:44:07 +0800

    chore: remove temp debug scripts

--- _v2.py
+++
@@ -1,6 +0,0 @@
-MOJI = ["鍒", "閸", "鎭", "鈺", "鈥", "婊", "鐧", "浣", "鏄"]
-for f in ["roadmap.html", "feedback.html", "about.html", "knowledge.html", "chat.html"]:
-    p = "src/cagent_os/interfaces/http/static/pages/" + f
-    d = open(p, encoding="utf-8").read()
-    hits = [c for c in MOJI if c in d]
-    print(f, "clean" if not hits else f"MOJIBAKE {hits[:3]}")
